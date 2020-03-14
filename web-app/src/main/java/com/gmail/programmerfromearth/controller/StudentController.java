4759a198b11ed4a44be751e2005777ec6abcdfc9
programmerfromearth <programmerfromearth@gmail.com>
2020-03-14 17:06:51 +0300

    Added functionality to add student in list

--- web-app/src/main/java/com/gmail/programmerfromearth/controller/StudentController.java
+++ web-app/src/main/java/com/gmail/programmerfromearth/controller/StudentController.java
@@ -1,11 +1,13 @@
 package com.gmail.programmerfromearth.controller;
 
 import com.gmail.programmerfromearth.dao.StudentDao;
+import com.gmail.programmerfromearth.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -16,13 +18,26 @@ public class StudentController {
         this.studentDao = studentDao;
     }
 
-    @RequestMapping(value = "/students")
+    @GetMapping(value = "/students")
     public String getStudent(Model model) {
         model.addAttribute("students", studentDao.getStudents());
         return "students";
     }
 
-    @RequestMapping(value = "/students/{id}/delete")
+    @GetMapping(value = "/student")
+    public String goToAddStudent(Model model) {
+        model.addAttribute("isNew", true);
+        model.addAttribute("student", new Student());
+        return "student";
+    }
+
+    @PostMapping(value = "/student")
+    public String addStudent(Student student) {
+        studentDao.addStudent(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping(value = "/students/{id}/delete")
     public String deleteStudents(@PathVariable Integer id, Model model) {
         studentDao.deleteStudent(id);
         return "redirect:/students";
